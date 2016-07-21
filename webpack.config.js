979e125325fb5cb94fca363c938ad5df1b5d5bb4
Mohamed GHARSALLAH <gmedhoover.gmail.com>
2016-07-22 01:49:22 +0200

    add step-2 logic

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack configuration file
+module.exports = {
+    // Define entry point
+    entry: './app/app.js',
+    // And the output files
+    output: {
+        filename: 'public/assets/js/app.js'
+    }
+};
