946bfdbb3f5eecdc961cfc2e730fb47a9d364a68
Mohamed GHARSALLAH <gmedhoover.gmail.com>
2016-07-22 01:37:48 +0200

    add step-2 logic

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
+// This is the entry file
+
+// Import your dependencies with any pattern you prefer : require, import(ES6) or CommonJs
+var beautify = require('./helpers/beautify');
+
 var div = document.getElementById('content');
 
-div.innerHTML = div.innerHTML + 'WEBPACK';
+div.innerHTML = div.innerHTML + beautify('webpack');
---
+++ app/helpers/beautify/index.js
@@ -0,0 +1,7 @@
+// Import required dependencies per each Module
+var _ = require('lodash');
+
+// Only expose what you need
+module.exports = function (title) {
+    return '<strong>' + _.toUpper(title) + '</strong>';
+}
