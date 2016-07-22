d5fde1ee7e788820adf51ce7359e03a9c613c2b0
Mohamed GHARSALLAH <gmedhoover.gmail.com>
2016-07-22 10:11:02 +0200

    add setp-4 logic

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
+// ES6 Woo-Hoo !!
 // webpack configuration file
+var path = require('path');
+
 module.exports = {
     // Define entry point
     entry: './app/app.js',
     // And the output files
     output: {
-        filename: 'public/assets/js/app.js'
+        // Define base folder for output files
+        path: path.resolve(__dirname, 'public'),
+        // Define allowed path for url to access via webpack-dev-server
+        publicPath: '/assets/',
+        // Define filename based on webpack run type
+        filename: outputPath()
     },
     // The module Object contains Options affecting normal modules, like which one must be automatically loaded.
     module: {
-        // loaders Transforms resource file that follows the specified pattern "{test}", loader are assigned in order, seperated by '!'
+        // Preloaders are executed BEFORE Bundling
+        preLoaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'jscs-loader'}
+        ],
+        // loaders Transforms resource file that follows the specified pattern '{test}', loader are assigned in order, seperated by '!'
         loaders:[
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.css$/, loader: 'style-loader!css-loader'},
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
         ]
     }
 };
+
+function outputPath() {
+    return (process.env.NODE_ENV === 'development') ? 'app.js' : 'assets/js/app.js';
+}
